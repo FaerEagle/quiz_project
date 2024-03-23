7e9e2acf727f797e0334b80f6a81fd9281ef2d3e
FaerEagle <igran2001@gmail.com>
2024-03-24 02:15:41 +0700

    add action link on result page

---
+++ scripts/check-result.js
@@ -0,0 +1,9 @@
+(function () {
+    const Check = {
+        init() {
+            //Данные об ответах берем отсюда https://testologia.site/get-quiz-right?id=1
+        }
+    }
+
+    Check.init();
+})();
--- scripts/result.js
+++ scripts/result.js
@@ -2,8 +2,14 @@
     const Result = {
         init() {
             const url = new URL(location.href);
-            document.getElementById('result-score').innerText = url.searchParams.get('score') +
-                '/' + url.searchParams.get('total');
+            const score = url.searchParams.get('score');
+            const total = url.searchParams.get('total');
+            if (score && total) {
+                document.getElementById('result-score').innerText = score +
+                    '/' + total;
+            } else {
+                location.href = 'index.html';
+            }
         }
     }
 
